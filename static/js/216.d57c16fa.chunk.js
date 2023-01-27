3f76cad7625e9459670ed6c5146f0a88d10af837
Olexii-US <106531559+Olexii-US@users.noreply.github.com>
2023-01-27 22:21:30 +0000

    Deploying to gh-pages from @ Olexii-US/goit-react-hw-05-movies@e5b1de243f46437ad5fd2e57d0c849afbfe7bba8 🚀

---
+++ static/js/216.d57c16fa.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[216],{7216:function(e,r,t){t.r(r);var a=t(184);r.default=function(){return(0,a.jsx)("div",{children:"Sorry, page NOT found"})}}}]);
+//# sourceMappingURL=216.d57c16fa.chunk.js.map
